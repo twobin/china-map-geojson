0d448cdce1adf56899bee321c1a006bc165b8d3e
twobin <twobin@live.com>
2017-02-15 16:48:19 +0800

    fix:update index

--- src/index.js
+++ src/index.js
@@ -1,45 +1,85 @@
-import China from './china.json';
-import Anhui from './province/an_hui_geo.json';
-import Aomen from './province/ao_men_geo.json';
-import Beijing from './province/bei_jing_geo.json';
-import Chongqing from './province/chong_qing_geo.json';
-import Fujian from './province/fu_jian_geo.json';
-import Gansu from './province/gan_su_geo.json';
-import Guangdong from './province/guang_dong_geo.json';
-import Guangxi from './province/guang_xi_geo.json';
-import Guizhou from './province/gui_zhou_geo.json';
-
-import Hainan from './province/hai_nan_geo.json';
-import Hebei from './province/he_bei_geo.json';
-import Henan from './province/he_nan_geo.json';
-import Heilongjiang from './province/hei_long_jiang_geo.json';
-import Hubei from './province/hu_bei_geo.json';
-import Hunan from './province/hu_nan_geo.json';
-import Jilin from './province/ji_lin_geo.json';
-import Jiangsu from './province/jiang_su_geo.json';
-import Jiangxi from './province/jiang_xi_geo.json';
-import Liaoning from './province/liao_ning_geo.json';
-
-import Neimenggu from './province/nei_meng_gu_geo.json';
-import Ningxia from './province/ning_xia_geo.json';
-import Qinghai from './province/qing_hai_geo.json';
-import Shandong from './province/shan_dong_geo.json';
-import Shanxi_1 from './province/shan_xi_1_geo.json';
-import Shanxi_3 from './province/shan_xi_3_geo.json';
-import Shanghai from './province/shang_hai_geo.json';
-import Sichuan from './province/si_chuan_geo.json';
-import Taiwan from './province/tai_wan_geo.json';
-
-import Tianjin from './province/tian_jin_geo.json';
-import Xizang from './province/xi_zang_geo.json';
-import Xianggang from './province/xiang_gang_geo.json';
-import Xinjiang from './province/xin_jiang_geo.json';
-import Yunnan from './province/yun_nan_geo.json';
-import Zhejiang from './province/zhe_jiang_geo.json';
+"use strict";
 
 module.exports = {
-  China, Anhui, Aomen, Beijing, Chongqing, Fujian, Guizhou, Guangxi, Guangdong,
-  Gansu, Hunan, Hubei, Heilongjiang, Henan, Hainan, Jiangxi, Jiangsu, Jilin,
-  Liaoning, Ningxia, Neimenggu, Qinghai, Sichuan, Shanghai, Shandong, Shanxi_1,
-  Shanxi_3, Tianjin, Taiwan, Xinjiang, Xianggang, Xizang, Yunnan, Zhejiang
+  'China': require('./china.json'),
+  '安徽': require('./province/an_hui_geo.json'),
+  '澳门': require('./province/ao_men_geo.json'),
+  '北京': require('./province/bei_jing_geo.json'),
+  '重庆': require('./province/chong_qing_geo.json'),
+  '福建': require('./province/fu_jian_geo.json'),
+  '甘肃': require('./province/gan_su_geo.json'),
+  '广东': require('./province/guang_dong_geo.json'),
+  '广西': require('./province/guang_xi_geo.json'),
+  '贵州': require('./province/gui_zhou_geo.json'),
+  '海南': require('./province/hai_nan_geo.json'),
+  '河北': require('./province/he_bei_geo.json'),
+  '河南': require('./province/he_nan_geo.json'),
+  '黑龙江': require('./province/hei_long_jiang_geo.json'),
+  '湖北': require('./province/hu_bei_geo.json'),
+  '湖南': require('./province/hu_nan_geo.json'),
+  '吉林': require('./province/ji_lin_geo.json'),
+  '江苏': require('./province/jiang_su_geo.json'),
+  '江西': require('./province/jiang_xi_geo.json'),
+  '辽宁': require('./province/liao_ning_geo.json'),
+  '内蒙古': require('./province/nei_meng_gu_geo.json'),
+  '宁夏': require('./province/ning_xia_geo.json'),
+  '青海': require('./province/qing_hai_geo.json'),
+  '山东': require('./province/shan_dong_geo.json'),
+  '陕西': require('./province/shan_xi_3_geo.json'),
+  '山西': require('./province/shan_xi_1_geo.json'),
+  '上海': require('./province/shang_hai_geo.json'),
+  '四川': require('./province/si_chuan_geo.json'),
+  '台湾': require('./province/tai_wan_geo.json'),
+  '天津': require('./province/tian_jin_geo.json'),
+  '西藏': require('./province/xi_zang_geo.json'),
+  '香港': require('./province/xiang_gang_geo.json'),
+  '新疆': require('./province/xin_jiang_geo.json'),
+  '云南': require('./province/yun_nan_geo.json'),
+  '浙江': require('./province/zhe_jiang_geo.json')
 };
+
+// import China from './china.json';
+// import Anhui from './province/an_hui_geo.json';
+// import Aomen from './province/ao_men_geo.json';
+// import Beijing from './province/bei_jing_geo.json';
+// import Chongqing from './province/chong_qing_geo.json';
+// import Fujian from './province/fu_jian_geo.json';
+// import Gansu from './province/gan_su_geo.json';
+// import Guangdong from './province/guang_dong_geo.json';
+// import Guangxi from './province/guang_xi_geo.json';
+// import Guizhou from './province/gui_zhou_geo.json';
+
+// import Hainan from './province/hai_nan_geo.json';
+// import Hebei from './province/he_bei_geo.json';
+// import Henan from './province/he_nan_geo.json';
+// import Heilongjiang from './province/hei_long_jiang_geo.json';
+// import Hubei from './province/hu_bei_geo.json';
+// import Hunan from './province/hu_nan_geo.json';
+// import Jilin from './province/ji_lin_geo.json';
+// import Jiangsu from './province/jiang_su_geo.json';
+// import Jiangxi from './province/jiang_xi_geo.json';
+// import Liaoning from './province/liao_ning_geo.json';
+
+// import Neimenggu from './province/nei_meng_gu_geo.json';
+// import Ningxia from './province/ning_xia_geo.json';
+// import Qinghai from './province/qing_hai_geo.json';
+// import Shandong from './province/shan_dong_geo.json';
+// import Shanxi_1 from './province/shan_xi_1_geo.json';
+// import Shanxi_3 from './province/shan_xi_3_geo.json';
+// import Shanghai from './province/shang_hai_geo.json';
+// import Sichuan from './province/si_chuan_geo.json';
+// import Taiwan from './province/tai_wan_geo.json';
+
+// import Tianjin from './province/tian_jin_geo.json';
+// import Xizang from './province/xi_zang_geo.json';
+// import Xianggang from './province/xiang_gang_geo.json';
+// import Xinjiang from './province/xin_jiang_geo.json';
+// import Yunnan from './province/yun_nan_geo.json';
+// import Zhejiang from './province/zhe_jiang_geo.json';
+
+// module.exports = {
+//   China, Anhui, Aomen, Beijing, Chongqing, Fujian, Guizhou, Guangxi, Guangdong,
+//   Gansu, Hunan, Hubei, Heilongjiang, Henan, Hainan, Jiangxi, Jiangsu, Jilin,
+//   Liaoning, Ningxia, Neimenggu, Qinghai, Sichuan, Shanghai, Shandong, Shanxi_1,
+//   Shanxi_3, Tianjin, Taiwan, Xinjiang, Xianggang, Xizang, Yunnan, Zhejiang
+// };
