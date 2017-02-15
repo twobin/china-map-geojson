599aeac0b47edc764523fd8b5114a9bc57967ce4
twobin <twobin@live.com>
2017-02-15 16:28:49 +0800

    feat:init project

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # china-map-geojson
+
 china map geojson
+
+[![npm version](https://badge.fury.io/js/china-map-geojson.png)](https://badge.fury.io/js/china-map-geojson)
+[![npm downloads](https://img.shields.io/npm/dt/china-map-geojson.svg?style=flat-square)](https://www.npmjs.com/package/china-map-geojson)
+
+## usage
+
+```
+$ npm i -S china-map-geojson
+```
+
+## docs
+
+### china-map-geojson
+
+```
+import { China, Zhejiang } from 'china-map-geojson';
+```
---
+++ src/index.js
@@ -0,0 +1,45 @@
+import China from './china.json';
+import Anhui from './province/an_hui_geo.json';
+import Aomen from './province/ao_men_geo.json';
+import Beijing from './province/bei_jing_geo.json';
+import Chongqing from './province/chong_qing_geo.json';
+import Fujian from './province/fu_jian_geo.json';
+import Gansu from './province/gan_su_geo.json';
+import Guangdong from './province/guang_dong_geo.json';
+import Guangxi from './province/guang_xi_geo.json';
+import Guizhou from './province/gui_zhou_geo.json';
+
+import Hainan from './province/hai_nan_geo.json';
+import Hebei from './province/he_bei_geo.json';
+import Henan from './province/he_nan_geo.json';
+import Heilongjiang from './province/hei_long_jiang_geo.json';
+import Hubei from './province/hu_bei_geo.json';
+import Hunan from './province/hu_nan_geo.json';
+import Jilin from './province/ji_lin_geo.json';
+import Jiangsu from './province/jiang_su_geo.json';
+import Jiangxi from './province/jiang_xi_geo.json';
+import Liaoning from './province/liao_ning_geo.json';
+
+import Neimenggu from './province/nei_meng_gu_geo.json';
+import Ningxia from './province/ning_xia_geo.json';
+import Qinghai from './province/qing_hai_geo.json';
+import Shandong from './province/shan_dong_geo.json';
+import Shanxi_1 from './province/shan_xi_1_geo.json';
+import Shanxi_3 from './province/shan_xi_3_geo.json';
+import Shanghai from './province/shang_hai_geo.json';
+import Sichuan from './province/si_chuan_geo.json';
+import Taiwan from './province/tai_wan_geo.json';
+
+import Tianjin from './province/tian_jin_geo.json';
+import Xizang from './province/xi_zang_geo.json';
+import Xianggang from './province/xiang_gang_geo.json';
+import Xinjiang from './province/xin_jiang_geo.json';
+import Yunnan from './province/yun_nan_geo.json';
+import Zhejiang from './province/zhe_jiang_geo.json';
+
+module.exports = {
+  China, Anhui, Aomen, Beijing, Chongqing, Fujian, Guizhou, Guangxi, Guangdong,
+  Gansu, Hunan, Hubei, Heilongjiang, Henan, Hainan, Jiangxi, Jiangsu, Jilin,
+  Liaoning, Ningxia, Neimenggu, Qinghai, Sichuan, Shanghai, Shandong, Shanxi_1,
+  Shanxi_3, Tianjin, Taiwan, Xinjiang, Xianggang, Xizang, Yunnan, Zhejiang
+};
