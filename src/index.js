ff2cb16365b746aac52a28c760dce566aaef1d0b
twobin <twobin@live.com>
2017-10-15 17:50:16 +0800

    fix:update index

--- src/index.js
+++ src/index.js
@@ -1,45 +1,6 @@
 import China from './china';
-import Anhui from './province/an_hui_geo';
-import Aomen from './province/ao_men_geo';
-import Beijing from './province/bei_jing_geo';
-import Chongqing from './province/chong_qing_geo';
-import Fujian from './province/fu_jian_geo';
-import Gansu from './province/gan_su_geo';
-import Guangdong from './province/guang_dong_geo';
-import Guangxi from './province/guang_xi_geo';
-import Guizhou from './province/gui_zhou_geo';
-
-import Hainan from './province/hai_nan_geo';
-import Hebei from './province/he_bei_geo';
-import Henan from './province/he_nan_geo';
-import Heilongjiang from './province/hei_long_jiang_geo';
-import Hubei from './province/hu_bei_geo';
-import Hunan from './province/hu_nan_geo';
-import Jilin from './province/ji_lin_geo';
-import Jiangsu from './province/jiang_su_geo';
-import Jiangxi from './province/jiang_xi_geo';
-import Liaoning from './province/liao_ning_geo';
-
-import Neimenggu from './province/nei_meng_gu_geo';
-import Ningxia from './province/ning_xia_geo';
-import Qinghai from './province/qing_hai_geo';
-import Shandong from './province/shan_dong_geo';
-import Shanxi_1 from './province/shan_xi_1_geo';
-import Shanxi_3 from './province/shan_xi_3_geo';
-import Shanghai from './province/shang_hai_geo';
-import Sichuan from './province/si_chuan_geo';
-import Taiwan from './province/tai_wan_geo';
-
-import Tianjin from './province/tian_jin_geo';
-import Xizang from './province/xi_zang_geo';
-import Xianggang from './province/xiang_gang_geo';
-import Xinjiang from './province/xin_jiang_geo';
-import Yunnan from './province/yun_nan_geo';
-import Zhejiang from './province/zhe_jiang_geo';
+import Province from './province';
 
 export {
-  China, Anhui, Aomen, Beijing, Chongqing, Fujian, Guizhou, Guangxi, Guangdong,
-  Gansu, Hunan, Hubei, Heilongjiang, Henan, Hainan, Jiangxi, Jiangsu, Jilin,
-  Liaoning, Ningxia, Neimenggu, Qinghai, Sichuan, Shanghai, Shandong, Shanxi_1,
-  Shanxi_3, Tianjin, Taiwan, Xinjiang, Xianggang, Xizang, Yunnan, Zhejiang
+  China, Province
 };
---
+++ src/province/index.js
@@ -0,0 +1,44 @@
+import Anhui from './an_hui_geo';
+import Aomen from './ao_men_geo';
+import Beijing from './bei_jing_geo';
+import Chongqing from './chong_qing_geo';
+import Fujian from './fu_jian_geo';
+import Gansu from './gan_su_geo';
+import Guangdong from './guang_dong_geo';
+import Guangxi from './guang_xi_geo';
+import Guizhou from './gui_zhou_geo';
+
+import Hainan from './hai_nan_geo';
+import Hebei from './he_bei_geo';
+import Henan from './he_nan_geo';
+import Heilongjiang from './hei_long_jiang_geo';
+import Hubei from './hu_bei_geo';
+import Hunan from './hu_nan_geo';
+import Jilin from './ji_lin_geo';
+import Jiangsu from './jiang_su_geo';
+import Jiangxi from './jiang_xi_geo';
+import Liaoning from './liao_ning_geo';
+
+import Neimenggu from './nei_meng_gu_geo';
+import Ningxia from './ning_xia_geo';
+import Qinghai from './qing_hai_geo';
+import Shandong from './shan_dong_geo';
+import Shanxi_1 from './shan_xi_1_geo';
+import Shanxi_3 from './shan_xi_3_geo';
+import Shanghai from './shang_hai_geo';
+import Sichuan from './si_chuan_geo';
+import Taiwan from './tai_wan_geo';
+
+import Tianjin from './tian_jin_geo';
+import Xizang from './xi_zang_geo';
+import Xianggang from './xiang_gang_geo';
+import Xinjiang from './xin_jiang_geo';
+import Yunnan from './yun_nan_geo';
+import Zhejiang from './zhe_jiang_geo';
+
+export {
+  Anhui, Aomen, Beijing, Chongqing, Fujian, Guizhou, Guangxi, Guangdong,
+  Gansu, Hunan, Hubei, Heilongjiang, Henan, Hainan, Jiangxi, Jiangsu, Jilin,
+  Liaoning, Ningxia, Neimenggu, Qinghai, Sichuan, Shanghai, Shandong, Shanxi_1,
+  Shanxi_3, Tianjin, Taiwan, Xinjiang, Xianggang, Xizang, Yunnan, Zhejiang
+};
